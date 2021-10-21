baa09a353de97853aba9d0b326d65de329e46b9f
Kevin Darlington <kevin@outroot.com>
2021-10-21 11:33:10 -0400

    fix: comment out old examples

--- examples/holdings.rs
+++ examples/holdings.rs
@@ -1,48 +1,48 @@
-use std::fs;
+// use std::fs;
 
-use personalcapital::types;
-use serde_json;
+// use personalcapital::types;
+// use serde_json;
 
-fn main() {
-  let json = fs::read_to_string("./.secret/getHoldings.json").unwrap();
-  let v: types::Response = serde_json::from_str(&json).unwrap();
-  let v: types::Holdings = serde_json::from_str(v.sp_data.get()).unwrap();
+// fn main() {
+//   let json = fs::read_to_string("./.secret/getHoldings.json").unwrap();
+//   let v: types::Response = serde_json::from_str(&json).unwrap();
+//   let v: types::Holdings = serde_json::from_str(v.sp_data.get()).unwrap();
 
-  let mut holdings: Vec<types::Holding> = v
-    .holdings
-    .into_iter()
-    .filter(
-      |x| /*x.source != types::HoldingSource::User && x.value >= 100.0 && */x.ticker.is_some(),
-    )
-    .collect();
-  holdings.sort_by(|a, b| b.value.partial_cmp(&a.value).unwrap());
-  let total: f64 = holdings.iter().map(|x| x.value).sum();
+//   let mut holdings: Vec<types::Holding> = v
+//     .holdings
+//     .into_iter()
+//     .filter(
+//       |x| /*x.source != types::HoldingSource::User && x.value >= 100.0 && */x.ticker.is_some(),
+//     )
+//     .collect();
+//   holdings.sort_by(|a, b| b.value.partial_cmp(&a.value).unwrap());
+//   let total: f64 = holdings.iter().map(|x| x.value).sum();
 
-  // println!("total: {}", total);
-  let mut hp = 0.0;
-  println!("Ticker,Dollar Amount");
-  for h in holdings {
-    println!(
-      "{},{}",
-      h.ticker.unwrap(),
-      // h.quantity,
-      h.value,
-      // h.value / total * 100.0,
-    );
-  }
+//   // println!("total: {}", total);
+//   let mut hp = 0.0;
+//   println!("Ticker,Dollar Amount");
+//   for h in holdings {
+//     println!(
+//       "{},{}",
+//       h.ticker.unwrap(),
+//       // h.quantity,
+//       h.value,
+//       // h.value / total * 100.0,
+//     );
+//   }
 
-  // for h in v.holdings {
-  //   if h.source == types::HoldingSource::User {
-  //     continue;
-  //   }
+//   // for h in v.holdings {
+//   //   if h.source == types::HoldingSource::User {
+//   //     continue;
+//   //   }
 
-  //   if h.value < 100.0 {
-  //     continue;
-  //   }
+//   //   if h.value < 100.0 {
+//   //     continue;
+//   //   }
 
-  //   if let Some(ticker) = h.ticker {
-  //     total += h.value;
-  //     println!("{} - {}", ticker, h.value);
-  //   }
-  // }
-}
+//   //   if let Some(ticker) = h.ticker {
+//   //     total += h.value;
+//   //     println!("{} - {}", ticker, h.value);
+//   //   }
+//   // }
+// }
--- examples/investment-type-total.rs
+++ examples/investment-type-total.rs
@@ -1,54 +1,54 @@
-use std::fs;
+// use std::fs;
 
-use personalcapital::pc_types;
-use serde_json;
+// use personalcapital::pc_types;
+// use serde_json;
 
-fn main() {
-  let json = fs::read_to_string("./.secret/getAccounts2.json").unwrap();
-  let v: pc_types::Response = serde_json::from_str(&json).unwrap();
-  let v: pc_types::Accounts = serde_json::from_str(v.sp_data.get()).unwrap();
+// fn main() {
+//   let json = fs::read_to_string("./.secret/getAccounts2.json").unwrap();
+//   let v: pc_types::Response = serde_json::from_str(&json).unwrap();
+//   let v: pc_types::Accounts = serde_json::from_str(v.sp_data.get()).unwrap();
 
-  let mut taxable: f64 = 0.0;
-  let mut tax_deferred: f64 = 0.0;
-  let mut tax_free: f64 = 0.0;
+//   let mut taxable: f64 = 0.0;
+//   let mut tax_deferred: f64 = 0.0;
+//   let mut tax_free: f64 = 0.0;
 
-  for account in v.accounts {
-    if account.product_type != pc_types::ProductType::Investment
-      || account.is_exclude_from_household
-    {
-      continue;
-    }
+//   for account in v.accounts {
+//     if account.product_type != pc_types::ProductType::Investment
+//       || account.is_exclude_from_household
+//     {
+//       continue;
+//     }
 
-    match (account.account_type_new, account.account_type_subtype) {
-      (pc_types::AccountTypeNew::Investment, pc_types::AccountTypeSubtype::None) => {
-        taxable += account.balance.unwrap()
-      },
-      (pc_types::AccountTypeNew::Ira, pc_types::AccountTypeSubtype::Roth) => {
-        tax_free += account.balance.unwrap()
-      },
-      (pc_types::AccountTypeNew::Ira, pc_types::AccountTypeSubtype::Traditional) => {
-        tax_deferred += account.balance.unwrap()
-      },
-      (pc_types::AccountTypeNew::A401k, pc_types::AccountTypeSubtype::Traditional) => {
-        tax_deferred += account.balance.unwrap()
-      },
-      (pc_types::AccountTypeNew::A401k, pc_types::AccountTypeSubtype::Roth) => {
-        tax_free += account.balance.unwrap()
-      },
-      (..) => {},
-    };
-    // println!(
-    //   "{}; {:?}; {:?}; {:?}; {:?}",
-    //   account.name,
-    //   account.account_type_group,
-    //   account.account_type,
-    //   account.account_type_new,
-    //   account.account_type_subtype
-    // );
-  }
+//     match (account.account_type_new, account.account_type_subtype) {
+//       (pc_types::AccountTypeNew::Investment, pc_types::AccountTypeSubtype::None) => {
+//         taxable += account.balance.unwrap()
+//       },
+//       (pc_types::AccountTypeNew::Ira, pc_types::AccountTypeSubtype::Roth) => {
+//         tax_free += account.balance.unwrap()
+//       },
+//       (pc_types::AccountTypeNew::Ira, pc_types::AccountTypeSubtype::Traditional) => {
+//         tax_deferred += account.balance.unwrap()
+//       },
+//       (pc_types::AccountTypeNew::A401k, pc_types::AccountTypeSubtype::Traditional) => {
+//         tax_deferred += account.balance.unwrap()
+//       },
+//       (pc_types::AccountTypeNew::A401k, pc_types::AccountTypeSubtype::Roth) => {
+//         tax_free += account.balance.unwrap()
+//       },
+//       (..) => {},
+//     };
+//     // println!(
+//     //   "{}; {:?}; {:?}; {:?}; {:?}",
+//     //   account.name,
+//     //   account.account_type_group,
+//     //   account.account_type,
+//     //   account.account_type_new,
+//     //   account.account_type_subtype
+//     // );
+//   }
 
-  println!("Taxable: {}", taxable);
-  println!("Tax Deferred: {}", tax_deferred);
-  println!("Tax Free: {}", tax_free);
-  println!("Total: {}", taxable + tax_deferred + tax_free);
-}
+//   println!("Taxable: {}", taxable);
+//   println!("Tax Deferred: {}", tax_deferred);
+//   println!("Tax Free: {}", tax_free);
+//   println!("Total: {}", taxable + tax_deferred + tax_free);
+// }
